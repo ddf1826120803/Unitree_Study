7fd921c7b8ae1a072abde53fc8923b7aa3c7c960
Dongfang Du <dongfangdu@google.com>
2025-02-19 17:38:45 +0800

    add g1 arm example

---
+++ arm/g1_arm7_sdk_dds_example.cpp
@@ -0,0 +1,89 @@
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <unitree/idl/hg/LowCmd_.hpp>
+#include <unitree/idl/hg/LowState_.hpp>
+#include <unitree/robot/channel/channel_publisher.hpp>
+#include <unitree/robot/channel/channel_subscriber.hpp>
+
+static const std::string kTopicArmSDK = "rt/arm_sdk";
+static const std::string kTopicState = "rt/lowstate";
+
+enum JointIndex {
+  // Left arm
+  kLeftShoulderPitch,
+  kLeftShoulderRoll,
+  kLeftShoulderYaw,
+  kLeftElbow,
+  kLeftWistRoll,
+  kLeftWistPitch,
+  kLeftWistYaw,
+  // Right arm
+  kRightShoulderPitch,
+  kRightShoulderRoll,
+  kRightShoulderYaw,
+  kRightElbow,
+  kRightWistRoll,
+  kRightWistPitch,
+  kRightWistYaw,
+};
+
+int main(int argc, char const *argv[]) {
+
+  // Subscribe to the topic message of "rt/arm_sdk"
+  unitree::robot::ChannelFactory::Instance()->Init(1, "lo");
+  unitree::robot::ChannelPublisherPtr<unitree_hg::msg::dds_::LowCmd_>
+      arm_sdk_publisher;
+   unitree_hg::msg::dds_::LowCmd_ msg;
+   arm_sdk_publisher.reset(
+      new unitree::robot::ChannelPublisher<unitree_hg::msg::dds_::LowCmd_>(
+          kTopicArmSDK));
+  arm_sdk_publisher->InitChannel();
+
+  // create subscriber
+  unitree::robot::ChannelSubscriberPtr<unitree_hg::msg::dds_::LowState_>
+      low_state_subscriber;
+  unitree_hg::msg::dds_::LowState_ state_msg;
+  low_state_subscriber.reset(
+      new unitree::robot::ChannelSubscriber<unitree_hg::msg::dds_::LowState_>(
+          kTopicState));
+  low_state_subscriber->InitChannel([&](const void *msg) {
+        auto s = ( const unitree_hg::msg::dds_::LowState_* )msg;
+        memcpy( &state_msg, s, sizeof( unitree_hg::msg::dds_::LowState_ ) );
+  }, 1);
+
+  std::array<JointIndex, 17> arm_joints = {
+      JointIndex::kLeftShoulderPitch,  JointIndex::kLeftShoulderRoll,
+      JointIndex::kLeftShoulderYaw,    JointIndex::kLeftElbow,
+      JointIndex::kLeftWistRoll,       JointIndex::kLeftWistPitch,
+      JointIndex::kLeftWistYaw,
+      JointIndex::kRightShoulderPitch, JointIndex::kRightShoulderRoll,
+      JointIndex::kRightShoulderYaw,   JointIndex::kRightElbow,
+      JointIndex::kRightWistRoll,      JointIndex::kRightWistPitch,
+      JointIndex::kRightWistYaw,
+      JointIndex::kWaistYaw,
+      JointIndex::kWaistRoll,
+      JointIndex::kWaistPitch};
+
+
+  // wait for init
+  std::cout << "Press ENTER to init arms ...";
+  std::cin.get();
+
+  // get current joint position
+  std::array<float, 17> current_jpos{};
+  std::cout <<"Current joint position: ";
+  for(int i = 0; i < arm_joints.size(); i++){
+    current_jpos.at(i) = state_msg.motor_state().at(arm_joints.at(i)).q();
+    std::cout << current_jpos.at(i) << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "All motor states:" << std::endl;
+  std::cout << state_msg.motor_state() << std::endl;
+
+  std::cout << "Done!" << std::endl;
+  return 0;
+}
